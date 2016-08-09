4982711486fc3c9bb782d830cb37243b910b0e90
BombInside <xtc.inside@gmail.com>
2016-08-10 01:58:04 +0300

    add partners

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :partners
+
   resources :news
 
   mount Ckeditor::Engine => '/ckeditor'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809222250) do
+ActiveRecord::Schema.define(version: 20160809225622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,13 @@ ActiveRecord::Schema.define(version: 20160809222250) do
     t.string   "title"
     t.text     "body"
     t.boolean  "visible"
+    t.text     "desc"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "partners", force: true do |t|
+    t.string   "title"
     t.text     "desc"
     t.datetime "created_at"
     t.datetime "updated_at"
